12e56018ce3e4a287b28827e47c63b7155cfe801
esonderegger <evan.sonderegger@gmail.com>
2014-03-02 18:07:42 -0500

    add navigation you server install script

--- install/install_youbot_server.sh
+++ install/install_youbot_server.sh
@@ -12,3 +12,4 @@ git clone -b hydro-devel https://github.com/ros-planning/moveit_commander.git
 git clone https://github.com/esonderegger/youbot_description.git
 git clone https://github.com/esonderegger/youbot_common.git
 git clone https://github.com/esonderegger/youbot_driver.git
+got clone https://github.com/esonderegger/youbot_navigation.git
