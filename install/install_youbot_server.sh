32c30b5cbe686a0774777fb7dc764da46ffff20d
esonderegger <evan.sonderegger@gmail.com>
2014-03-02 13:25:42 -0500

    added install script for server

---
+++ install/install_youbot_server.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# install_youbot.sh
+# run this to install necessary packages onto the Kuka install_youbot
+
+sudo apt-get install ros-hydro-octomap-rviz-plugins ros-hydro-octomap-mapping ros-hydro-orocos-kdl  ros-hydro-joy ros-hydro-moveit-full
+
+cd ~/catkin_ws/src
+
+git clone https://github.com/WPI-RAIL/brics_actuator.git
+git clone -b hydro-devel https://github.com/ros-drivers/hokuyo_node.git
+git clone -b hydro-devel https://github.com/ros-planning/moveit_commander.git
+git clone https://github.com/esonderegger/youbot_description.git
+git clone https://github.com/esonderegger/youbot_common.git
+git clone https://github.com/esonderegger/youbot_driver.git
